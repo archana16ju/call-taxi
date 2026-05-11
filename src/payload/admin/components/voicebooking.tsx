b4591de317d55cef574e51a138efa1848dd74d42
archana16ju <archana.work.in@gmail.com>
2026-05-11 12:39:39 +0530

    Initial commit

--- src/payload/admin/components/voicebooking.tsx
+++ src/payload/admin/components/voicebooking.tsx
@@ -88,7 +88,7 @@ export default function VoiceBookings() {
           </Typography>
 
           {/* LINK TO ALERT PAGE */}
-          <Link href="/collections/Alerts">
+          <Link href="/collections/alerts">
             <Button variant="contained">Go to Alerts</Button>
           </Link>
         </Stack>
